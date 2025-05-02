8cfcf45ba184b6a9c7fed255f008516a1ce70e40
nate-powers3 <nate.powers619@gmail.com>
2025-05-02 13:07:52 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,116 @@
+<script>
+function button(button) {
+	alert("Welcome to my website")
+}
+var one = ("welcome to my website");
+function button.onMouseDown() {
+   document.getElementById("#button");
+ }
+function Button(Button) {
+	alert("Welcome to my website")
+}
+var one = ("welcome to my website");
+Function button.onMouseDown() {
+   document.getElementById("#button");
+ }
+ var 2 = ARRAY.pop(1,2);
+parseInt()
+ var class = "two"
+ var class = "one";
+ ARRAY [1]
+ ARRAY ["SUN"]["MARS"]
+ A
+ function galaxy(galaxy) {
+      SWITCH []
+      slice(1,2)
+setTimeout(function() {}, 10);
+one.prototype.button1 = function(button) {
+    ARRAY.POP([1], "welcome to my website");
+    ARRAY.slice([1])
+    document.getElementById("one");
+    curr
+    if (Button.OnMouseDown())
+button2()
+if (Button.OnMouseDown()) == true {alert("welcome to my website!")} else {alert("button not working")}
+
+};
+method_name: function(attribute) {
+	
+},
+ getMethod()
+ var class = "one"
+parseInt
+ alert(class)
+Object {}
+alert: function(alerte) {
+	alert("welcome to my website")
+	
+},
+for (var i = 0; i < Things.length; i++) {
+	Things[i]
+}var 
+for (var i = Things.length - 1; i >= 0; i--) {
+	Things[i]
+}
+alert(Things[])
+Things[i]
+ function galaxy(galaxy1: function() {
+
+ switch {
+ 	case 1: 
+ }
+ break;
+ for (var i = Things.length - 1; i >= 0; i--) {
+  	Things[i]
+  } 
+  
+  for (var i = 0; i < Things.length; i++) {
+  	Things[i]
+  }
+ function Date(setDate) {
+ 	alert(date())
+ 	// body...
+ }
+ switch {
+ 	case: 1
+ }
+ if false
+ for (var i = 0; i < Things.length; i++) {
+ 	Things[i]
+ }
+var 
+for (var i = 0; i < Things.length; i++) {
+	Things[i]
+}
+
+switch {
+	case 1 = "one";
+	case 2 = "two";
+	case 3== "three";
+}
+function button() {
+  var Array = ["One", "two", "Three"].pop(1,2);
+   alert(Array);
+ }
+ button1()
+ function Date(date) {
+ 	date()
+ }
+Date()
+
+getElementsByTagName('')
+function (button) {
+}
+if {var i - 1
+alert("worked")4
+}
+else {
+alert("didnt work")}
+}
+var i = 1
+ </script>
+}
+}
+}
+}
+f
